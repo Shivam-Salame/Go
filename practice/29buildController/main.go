5803831c640db5e5f66e6274a015d812c338485b
shivam <shivam8463030933@gmail.com>
2024-04-09 23:21:13 +0530

    new files added

--- practice/29buildController/main.go
+++ practice/29buildController/main.go
@@ -3,7 +3,12 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"math/rand"
 	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gorilla/mux"
 )
 
 // model for course-file
@@ -26,7 +31,9 @@ var courses []Course
 
 // middlewear, helper-file
 func (c *Course) IsEmpty() bool {
-	return c.CourseId == "" && c.CourseName == ""
+	// return c.CourseId == "" && c.CourseName == ""
+	return c.CourseName == ""
+
 }
 
 // controllers-file
@@ -41,6 +48,50 @@ func getAllCourses(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("get all courses")
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(courses)
+}
+
+func getOneCourses(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("get one course")
+	w.Header().Set("Content-Type", "application/json")
+
+	// grab id from request
+
+	params := mux.Vars(r)
+	
+	// loop through courses, find matching id and return response
+	for _, course := range courses {
+		if course.CourseId == params["id"] {
+			json.NewEncoder(w).Encode(course)
+		}
+	}
+	json.NewEncoder(w).Encode("No course found with matching id")
+	return 
+}
+
+func createOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("create one course")
+	w.Header().Set("Content-Type", "application/json")
+
+	// if body is empty
+	if r.Body == nil {
+		json.NewEncoder(w).Encode("Please send some data")
+	}
+
+	var course Course
+	_ =json.NewDecoder(w).Decode(&course)
+	if course.IsEmpty() {
+		json.NewEncoder(w).Encode("No data inside json")
+		return
+	}
+
+	// generate unique id, string
+	// append course into courses
+
+	rand.Seed(time.Now().UnixNano())
+	course.CourseId = strconv.Itoa(rand.Intn(100))
+	courses = append(courses, course)
+	json.NewEncoder(w).Encode(course)
+	return
 }
 
 func main() {
